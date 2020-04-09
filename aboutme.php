c092eda795e5e97f9a362b50ab78557ff4ac140f
SefvianaAnggraeni <63381365+SefvianaAnggraeni@users.noreply.github.com>
2020-04-09 13:29:13 +0700

    Update aboutme.php

--- aboutme.php
+++ aboutme.php
@@ -1,5 +1,6 @@
 <?php echo "Nama : Sefviana Anggraeni";
 echo "Kelas : XI RPL 2";
 echo "Asal Sekolah : SMK Taruna Bhakti";
-echo "Alamat Rumah : Gang. H.Sainin Kp.Sugutamu Rt. 02 Rw. 28 No. 97 Kel. Baktijaya Kec. Sukmajaya Kota Depok";
+echo "No.HP : 089634536285";
+echo "Email : sefviaraenrais@gmail.com";
 ?>
