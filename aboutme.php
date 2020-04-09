87bf10f8fc2b5c41769b89d96a8c87035b65c80a
SefvianaAnggraeni <63381365+SefvianaAnggraeni@users.noreply.github.com>
2020-04-09 13:23:25 +0700

    Delete aboutme.php

--- aboutme.php
+++
@@ -1,4 +0,0 @@
-<?php echo "Nama : Sefviana Anggraeni";
-echo "Kelas : XI RPL 2";
-echo "Asal Sekolah : SMK Taruna Bhakti";
-echo "Alamat Rumah : Gang. H.Sainin Kp.Sugutamu Rt. 02 Rw. 28 No. 97 Kel. Baktijaya Kec. Sukmajaya Kota Depok"?>
